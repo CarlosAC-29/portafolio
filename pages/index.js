9f2727db69d5ce0367a3fd5446fb199809447a16
Carlos Cáceres <carlosmicro33@gmail.com>
2023-02-11 09:15:45 -0500

    add favIcon

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,10 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png"></link>
         <title> Carlos | Developer </title>
         <meta name="description" content="Hi, I'm Carlos 👋" />
       </Head>
